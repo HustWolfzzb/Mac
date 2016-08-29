b2a43c702418e38e918b5f633568ee1634361369
HustWolf <1184827350@qq.com>
2016-08-29 23:17:17 +0800

    javascript

---
+++ Javascript.js
@@ -0,0 +1,14 @@
+<script>
+function changeImage()
+{
+	element=document.getElementById('myimage')
+	if (element.src.match("xunruier2"))
+ 	{
+  		element.src="xunruier3.jpg";
+  	}
+	else
+   {
+  		element.src="xunruier2.jpg";
+   }
+}
+</script>
