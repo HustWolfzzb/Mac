14fc17906daf5bb76bcfde0936e9327ccecf45b6
HustWolf <1184827350@qq.com>
2016-09-18 23:35:11 +0800

    ok

---
+++ hello.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<meta charset="utf-8">
+<title>Hello World</title>
+</head>
+<body>
+	<div align="center"><h1>
+	<h1> Error 404 </h1>
+              <br>
+	<h3>	What you searach is not found </h3>
+		<h5>
+		"<?php 
+		echo &_REQUEST['searach'];  
+		?>"  you should find another way ... 
+		</h5>
+	</h1>
+	</div>
+</body>
+</html>
